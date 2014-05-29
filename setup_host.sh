48ccb45115b93a1e5f84e1e4ac1462a9beb1be76
sebastianhutter <shutter@Sebastians-MacBook-Pro-5.local>
2014-05-30 00:45:53 +0200

    selinux configuration done now the pam configuration needs to be corrected (or sudo nopassd)

--- setup_host.sh
+++ setup_host.sh
@@ -15,11 +15,7 @@ SUDOERSFILE=/etc/sudoers.d/ansible
 PAM=/etc/pam.d/sudo
 PUBLICKEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAqv5AqnC19iCzOFt+bpLgvs0y9gv2COrc0MLpmX6s3xJa5FPDZZpQiqCnQ94ik7AbJwjJImfyfTV8dx+1W+xUFr1252GO+0QHQC9uPI2rDL/KVKb9gHKZFGrXd9uPW3FlY66mWGtKMQYSqpsOpnkhqfHrj4gvi3MKCsJlackSsIw3+E19jMwkfPvQRvV1nWt60I1z9m4r4eHUYo1TZ3mY2yutJcusbHl36cSxFnu+5KGlGnnPaGZFEZYB24xloXRvwGx92eJ22jaiS17FT/IRQ2XGYuXNneNueKsSMD/V7G3CzZMrs69iGheh3V7phnKitA5A4IfH4NXoTFJjp5nMkw=="
 
-
-
 # the setup does the following
-# 1. create a new user under which ansible will execute its commands
-# 2. create a sudo configuration for the ansible setup
 
 # create the user account
 useradd -c 'ansible system user. used by our configuration management' -d $ANSIBLEHOME $USERNAME
@@ -27,6 +23,9 @@ if [[ $? -ne 0 ]]; then
 	echo "could not create user $USERNAME. aborting..." 1>&2
 	exit 2
 fi
+
+# set the selinux role for the ansible home directory
+chcon -h unconfined_u:object_r:user_home_dir_t:s0 /var/ansible     
 
 # create a sudoers configuration
 # the sudoers config will allow all users in the group 'ansible' to execute commands
@@ -53,19 +52,33 @@ echo %$USERNAME ALL=\(ALL\) ALL >> $SUDOERSFILE
 # now configure pam to enable sudo authentication via ssh
 # only the authorized keys for the ansible users are allowed
 # http://siliconexus.com/blog/2012/11/sudo-authentication-via-ssh-agent/comment-page-1/
-sed -i '2i\auth sufficient pam_ssh_agent_auth.so file=$ANSIBLEHOME/.ssh/authorized_keys' $PAM
+sed -i "2i\auth sufficient pam_ssh_agent_auth.so file=$ANSIBLEHOME/.ssh/authorized_keys" $PAM
 if [[ $? -ne 0 ]]; then
 	echo "could not add pam auth to '$PAM'. aborting ..." 1>&2
-	exit 4
+	exit 5
 fi
 
 # create the ssh directory for the ansible user
 mkdir $ANSIBLEHOME/.ssh
+if [[ $? -ne 0 ]]; then
+	echo "couldnt create .ssh directory in $ANSIBLEHOME. aborting ..." 1>&2
+	exit 6
+fi
+
+
 # add the public key for the user
 echo $PUBLICKEY > $ANSIBLEHOME/.ssh/authorized_keys
+if [[ $? -ne 0 ]]; then
+	echo "could not add the authoorized_keys file. aborting ..." 1>&2
+	exit 7
+fi
+
+
 # set the correct permissions
 chown -R $USERNAME:$USERNAME $ANSIBLEHOME/.ssh
 chmod 0700 $ANSIBLEHOME/.ssh
 chmod 0600 $ANSIBLEHOME/.ssh/authorized_keys
-
-# http://serverfault.com/questions/50573/selinux-preventing-passwordless-ssh-login
+if [[ $? -ne 0 ]]; then
+	echo "could not change permissions on .ssh directory. aborting ..." 1>&2
+	exit 8
+fi
