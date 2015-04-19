7ece920133db73a749fe67595b19234ab23cf51b
Sebastian Hutter <mail@sebastian-hutter.ch>
2015-04-19 15:15:14 +0200

    added queue cleanup script

---
+++ roles/transmission/templates/cleanup_transmission_queue.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this script stops the transmission daemon and cleans up torrents
+# it is possible that we loose an active download trough it but the risk is small
+
+logger -t "cleantransmissionqueue" "starting transmission queue cleanup script"
+# check if the download directory exist
+if [ -d "{{ queue }}" ]; then
+  systemctl stop transmission-daemon
+  # delete files
+  logger -t "cleantransmissionqueue" "check {{ queue }} for torrents older then 7 days"
+  find "{{ queue }}" -mmin +7d -delete -exec logger -t "cleantransmissionqueue" delete {} \;
+  systemctl start transmission-daemon
+fi
+logger -t "cleantransmissionqueue" "finished transmission queue cleanup script"
